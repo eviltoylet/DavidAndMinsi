207035f788085130ed82473ba4fabd54140ee621
Brian Chang <eviltoylet@hotmail.com>
2013-03-02 23:45:33 -0800

    allow submission only if form is completely filled

--- app/controllers/rsvp_controller.rb
+++ app/controllers/rsvp_controller.rb
@@ -9,12 +9,26 @@ class RsvpController < ApplicationController
       redirect_to "/rsvp/invalid?user=#{params[:user]}"
       return
     end
-    redirect_to "/rsvp/show?user=#{params[:user]}" unless @rsvp.words["13"].blank?
+    view_context.getmapping.each do |key, value|
+      if @rsvp.words[key].blank?
+        return
+      end
+    end
+
+    redirect_to "/rsvp/show?user=#{params[:user]}"
   end
 
   def update
     @rsvp = Rsvp.find_by_user params[:rsvp][:user]
     @rsvp.update_attributes(params[:rsvp])
+
+    view_context.getmapping.each do |key, value|
+      if params[:rsvp][:words][key].blank?
+        redirect_to "/rsvp/edit?user=#{params[:rsvp][:user]}"
+        return
+      end
+    end
+
     redirect_to "/rsvp/show?user=#{params[:rsvp][:user]}"
   end
 
