1af4a0f90bb30058b38e87dbddc33adc2cd4e76e
Brian Chang <eviltoylet@hotmail.com>
2013-04-13 14:53:50 -0700

    add missing method on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   serialize :words
   attr_accessible :custom_link, :first_name, :last_name, :guest_first_name, :guest_last_name, :attending, :comments, :words
+
+  def guest?
+    !guest_first_name.blank?
+  end
 end
